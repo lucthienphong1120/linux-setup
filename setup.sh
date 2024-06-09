af4747c1f722f47d0297e3bd8cb8f6169c481374
Thien Phong <aichoibangbangko@gmail.com>
2024-06-09 21:00:12 +0700

    add signal each echo

--- setup.sh
+++ setup.sh
@@ -6,25 +6,25 @@
 # =====================================================
 
 # Print banner
-echo "Initialize setup script..."
+echo "[Info] Initialize setup script..."
 
 # Check root running
 if [ "$EUID" -ne 0 ]; then
-  echo "Error: This script must be run as root or with sudo."
+  echo "[Error] This script must be run as root or sudo"
   exit 1
 else
-  echo "Check root privilege is OK"
+  echo "[OK] Check root privilege is available"
 fi
 
 # Get current OS and version
-echo "Get current OS and version..."
+echo "[Info] Get current OS and version..."
 OS_SUPPORT="Ubuntu"
 VERSION_SUPPORT=("22.04" "24.04")
 CURRENT_OS=$(lsb_release -si)
 CURRENT_VERSION=$(lsb_release -sr)
 
 # Function to check if CURRENT_VERSION is in the VERSION_SUPPORT array
-is_version_valid() {
+check_version() {
   local version=$1
   for v in "${VERSION_SUPPORT[@]}"; do
     if [[ "$v" == "$version" ]]; then
@@ -35,10 +35,10 @@ is_version_valid() {
 }
 
 # Check OS compatition
-if [[ "$CURRENT_OS" == "$OS_SUPPORT" ]] && is_version_valid "$CURRENT_VERSION"; then
-  echo "Current OS is $CURRENT_OS $CURRENT_VERSION"
+if [[ "$CURRENT_OS" == "$OS_SUPPORT" ]] && check_version "$CURRENT_VERSION"; then
+  echo "[OK] Current OS is $CURRENT_OS $CURRENT_VERSION"
 else
-  echo "This script is only available with $required_os versions ${required_versions[*]}"
+  echo "[Error] This script is only available with $required_os versions ${required_versions[*]}"
   exit 1
 fi
 
@@ -47,14 +47,14 @@ sleep 1
 # =====================================================
 
 # Find current hostname
-echo "Find current hostname..."
+echo "[Info] Find current hostname..."
 DEFAULT_HOSTNAME=$(hostname)
 
 # Set hostname
 read -p "Enter new hostname (default: $DEFAULT_HOSTNAME): " HOSTNAME
 HOSTNAME=${HOSTNAME:-$DEFAULT_HOSTNAME}
 
-echo "Set hostname to $HOSTNAME"
+echo "[OK] Set hostname to $HOSTNAME"
 
 hostnamectl set-hostname $HOSTNAME
 sed -i "s/127.0.1.1 $DEFAULT_HOSTNAME/127.0.1.1 $HOSTNAME/g" /etc/hosts
@@ -65,15 +65,15 @@ sleep 1
 # =====================================================
 
 # Find network config file
-echo "Find network config file..."
+echo "[Info] Find network config file..."
 NETWORK_FILE=$(find /etc/netplan -type f -name '*.yaml' | head -n1)
 
 if [ -z "$NETWORK_FILE" ]; then
-  echo "Error: No network config file found in /etc/netplan"
+  echo "[Error] No network config file found in /etc/netplan"
   exit 1
 fi
 
-echo "Found network config at $NETWORK_FILE"
+echo "[OK] Found network config at $NETWORK_FILE"
 
 # With /etc/netplan/00-installer-config.yaml template as follow:
 # ```
@@ -96,7 +96,7 @@ DEFAULT_GATEWAY=$(awk '/gateway4:/{print $2}' $NETWORK_FILE)
 DEFAULT_NAMESERVERS=$(awk '/nameservers:/ {getline; print $2,$3}' /etc/netplan/00-installer-config.yaml | tr -d '[]')
 
 if [ -z "$DEFAULT_IP" ] || [ -z "$DEFAULT_NETMASK" ] || [ -z "$DEFAULT_GATEWAY" ] || [ -z "$DEFAULT_NAMESERVERS" ]; then
-  echo "Failed to get network configuration from NETWORK_FILE"
+  echo "[Error] Failed to get network configuration from NETWORK_FILE"
   exit 1
 fi
 
@@ -109,7 +109,7 @@ GATEWAY=${GATEWAY:-$DEFAULT_GATEWAY}
 read -p "Enter new gateway (default: $DEFAULT_NAMESERVERS): " NAMESERVERS
 NAMESERVERS=${NAMESERVERS:-$DEFAULT_NAMESERVERS}
 
-echo "Configure static IP to $IP/$NETMASK $GATEWAY"
+echo "[OK] Configure static IP to $IP/$NETMASK $GATEWAY"
 
 cat <<EOF > $NETWORK_FILE
 network:
@@ -131,19 +131,19 @@ sleep 1
 # =====================================================
 
 # Check GPT PMBR size mismatch
-echo "Check GPT PMBR size..."
+echo "[Info] Check GPT PMBR size..."
 
 GPT_CHECK=$(fdisk -l 2>&1 | grep "GPT PMBR size mismatch")
 
 if [ -n "$GPT_CHECK" ]; then
   echo "$GPT_CHECK"
 else
-  echo "GPT PMBR is OK"
+  echo "[OK] GPT PMBR is OK, skipping this stage"
 fi
 
 # Resizing disk and LVM
 if [ -n "$GPT_CHECK" ]; then
-  echo "Resizing disk and LVM..."
+  echo "[OK] Resizing disk and LVM..."
   growpart /dev/sda 3
   lvextend -l +100%FREE /dev/ubuntu-vg/ubuntu-lv
 fi
@@ -153,7 +153,7 @@ sleep 1
 # =====================================================
 
 # Update package lists
-echo "Update package lists and upgrade packages..."
+echo "[Info] Update package lists and upgrade packages..."
 DEFAULT_UPDATE="n"
 DEFAULT_UPGRADE="n"
 
@@ -173,4 +173,4 @@ sleep 1
 
 # =====================================================
 
-echo "Setup complete!"
+echo "[Info] Setup complete!"
